bffa6fcff7cacc2f46704707489712d8ebfc3917
AnochAno <anothankes876@gmail.com>
2023-10-09 15:31:12 +0530

    loops

--- python/lean.py
+++ python/lean.py
@@ -1,5 +1,13 @@
-print ("hello")
-x = 15
-y = 25
+x = 101
 
-print(x+y)
+if x > 100:
+    k = x * 2
+    if x >= 200:
+        k = k * 3
+    elif k < 300:
+        k = k * 4
+    else:
+        k = k * 5
+else:
+    k = x * 3
+print (k)
