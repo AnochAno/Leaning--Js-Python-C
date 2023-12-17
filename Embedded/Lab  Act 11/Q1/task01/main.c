75c99a691d4e561a51665e2a6c3414989a2b88ae
AnochAno <anothankes876@gmail.com>
2023-12-17 20:22:18 +0530

    embedded

--- Embedded/Lab  Act 11/Q1/task01/main.c
+++ Embedded/Lab  Act 11/Q1/task01/main.c
@@ -3,27 +3,27 @@
 #include <util/delay.h>
 
 void displaySSD(unsigned int num) {
-	
+	// Define segment values for common cathode 7-segment display
 	char seg[] = {0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B};
 	PORTC = seg[num];
 }
 
 void init_ssd() {
-	DDRC = 0xFF; 
+	DDRC = 0xFF; // Set all PORTC pins as output
 }
 
 int main(void) {
-	init_ssd(); 
+	init_ssd(); // Call init_ssd function
 
 	unsigned int counter = 0;
 
 	while (1) {
-		displaySSD(counter % 20); 
+		displaySSD(counter % 20); // Display the least significant digit of the counter
 		_delay_ms(4000); // Delay to prevent ghosting
 		counter++;
 
 		if (counter == 10) {
-			counter = 0; 
+			counter = 0; // Reset counter when it reaches 10
 		}
 	}
 
