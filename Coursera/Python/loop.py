446150af434371b80d4b3e5a60b1459dc68f3f00
AnochAno <anothankes876@gmail.com>
2023-12-24 12:22:37 +0530

    py loop

--- Coursera/Python/loop.py
+++ Coursera/Python/loop.py
@@ -1,6 +1,6 @@
 #for loop
 #for i in range(10):
-    #print("loops...", i)
+##print("loops...", i)
 
 list1 = [1,2,3,40]
 list2 = [1,2,3,40]
@@ -8,8 +8,9 @@ list2 = [1,2,3,40]
 count = 0
 for x in list1:
     count += 1
+
 for x in list2:
     count += 1
-
-print(count)        
     
+print(count) 
+   
