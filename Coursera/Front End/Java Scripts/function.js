c6f01ed9e199141a17e61068ad3bac0ac528adc6
AnochAno <anothankes876@gmail.com>
2024-01-31 18:49:56 +0530

    prac

---
+++ Coursera/Front End/Java Scripts/function.js
@@ -0,0 +1,13 @@
+function add(){
+    var a = 10;
+    var b = 15;
+    var c = a + b;
+    console.log(c);
+}
+
+    add();
+
+ function kan(c ,b ){
+    var x = c+b;
+ } 
+ kan(6,7);  
---
+++ Embedded/Lab  Act 11/Q1/task 03/Tutorial/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 1/1/2024 4:31:05 PM
+ * Author : Anojan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
